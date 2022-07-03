51af11a37013eb1b29e0efa6facc073eddf9371c
ML6503 <ml6503@gmail.com>
2022-07-04 01:45:04 +0300

    fix: import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
 
   product: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev', 
-  import: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev/import',
+  import: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
