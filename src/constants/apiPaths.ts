2f2edc93fd05ae805029f470201d0194699e987d
ML6503 <ml6503@gmail.com>
2022-07-21 15:32:20 +0300

    fix: paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev', 
+  import: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
