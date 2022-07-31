b3e356d1dbc004e9dc3b0e8bbe91a318559ca0a4
ML6503 <ml6503@gmail.com>
2022-07-31 21:40:48 +0300

    feat: add cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev', 
   import: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  cart: 'http://ml6503-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
