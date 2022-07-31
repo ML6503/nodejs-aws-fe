6659fca23fc24a18303ee7716f3bf6b181c65953
ML6503 <ml6503@gmail.com>
2022-07-31 22:48:54 +0300

    fix: https issue

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   order: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev', 
   import: 'https://90m4tsqga1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rxqgzhje6j.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://ml6503-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
+  // cart: 'http://ml6503-cart-api-dev.eu-west-1.elasticbeanstalk.com/api' # not secure http converted to https below
+  cart: 'https://8k5p7io0i0.execute-api.eu-west-1.amazonaws.com/api'
 };
 
 export default API_PATHS;
